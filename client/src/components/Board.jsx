2ac24b6053099fccee352cdaf376506ef274c8b8
Saurabh <sauravthakulla683@gmail.com>
2026-01-01 23:00:49 +0530

    ADD : Responsive Board

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -4,17 +4,21 @@ const TOTAL_CELLS = ROWS * COLS;
 
 export const Board = () => {
   return (
-    <div className="flex justify-center items-center h-[70vh] bg-slate-900">
-      <div
-        className="grid w-[50vw] h-[60vh]"
-        style={{
-          gridTemplateColumns: `repeat(${COLS}, 1fr)`,
-          gridTemplateRows: `repeat(${ROWS}, 1fr)`,
-        }}
-      >
-        {Array.from({ length: TOTAL_CELLS }).map((_, i) => (
-          <div key={i} className="border border-slate-700 text-2xl" />
-        ))}
+    <div className="flex justify-center items-center min-h-[70vh] bg-gradient-to-b from-[#020617] via-[#020617] to-black text-green-400 font-mono">
+      <div className="flex justify-center items-center">
+        <div
+          className="
+            grid
+            w-[600px] h-[600px]
+            grid-cols-[repeat(20,1fr)]
+            grid-rows-[repeat(20,1fr)]
+            border border-slate-600
+          "
+        >
+          {Array.from({ length: TOTAL_CELLS }).map((_, i) => (
+            <div key={i} className="border border-slate-700" />
+          ))}
+        </div>
       </div>
     </div>
   );
